db2e91338830f2aa21278243cc9c621b7e9bcfa3
weihu <1007289387@qq.com>
2023-09-19 23:47:26 +0800

    blogs create

---
+++ blogs-admin/src/main/java/cn/zq/blogsadmin/BlogsAdminApplication.java
@@ -0,0 +1,13 @@
+package cn.zq.blogsadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogsAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogsAdminApplication.class, args);
+    }
+
+}
